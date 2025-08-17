86f0705a39f5cd0cc29020aba27729144a4131c4
Version Upgrade <n/a>
2025-08-17 23:20:25 +0000

    Automated commit for Q Code Transformation Job: 91b688ea-42cf-4b22-83fc-db46afc8e028

--- src/test/java/org/springframework/web/filter/Sha512ShallowEtagHeaderFilter.java
+++ src/test/java/org/springframework/web/filter/Sha512ShallowEtagHeaderFilter.java
@@ -5,7 +5,6 @@ import com.google.common.hash.Hashing;
 
 public class Sha512ShallowEtagHeaderFilter extends ShallowEtagHeaderFilter {
 
-	@Override
 	protected String generateETagHeaderValue(byte[] bytes) {
 		final HashCode hash = Hashing.sha512().hashBytes(bytes);
 		return "\"" + hash + "\"";
